be0bf53304048eadc990fa6989369d485da1c477
Robert <rbruce155@gmail.com>
2016-07-20 17:36:31 -0700

    Initial Commit

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -15,26 +15,21 @@ class Welcome(Controller):
         This is an example of loading a model.
         Every controller has access to the load_model method.
         """
-        self.load_model('WelcomeModel')
+        self.load_model('Course')
         self.db = self._app.db
 
-        """
-        
-        This is an example of a controller method that will load a view for the client 
-
-        """
-   
     def index(self):
         """
-        A loaded model is accessible through the models attribute 
+        A loaded model is accessible through the models attribute
         self.models['WelcomeModel'].get_users()
-        
+
         self.models['WelcomeModel'].add_message()
         # messages = self.models['WelcomeModel'].grab_messages()
         # user = self.models['WelcomeModel'].get_user()
         # to pass information on to a view it's the same as it was with Flask
-        
+
         # return self.load_view('index.html', messages=messages, user=user)
         """
-        return self.load_view('index.html')
 
+        allCourses = self.models['Course'].show_courses()
+        return self.load_view('index.html', allCourses=allCourses)
